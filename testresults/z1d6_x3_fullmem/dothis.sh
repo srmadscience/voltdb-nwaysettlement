3a02b89fd2e9c1aa99a4752be6df597d340a958f
David Rolfe <drolfe@voltdb.com>
2022-04-21 16:03:02 +0100

    remove obselete results

--- testresults/z1d6_x3_fullmem/dothis.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-for usercount in 2 3 4 5; do for i in 20 22 24 26 28 30 32 34 36 38 40  ; do java -jar TestClient.jar vdb1,vdb2,vdb3  $i 100000 9000000 15 15 $usercount 0 > ${i}_${usercount}.lst ; sleep 120; done; done 
--- testresults/z1d6_x5_5way/test1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for i in 24 26 28 30 32 34 36 38 40  ; do java -jar ../jars/TestClient.jar `cat /home/ubuntu/.vdbhostnames` $i 100000 9000000 15 15 5 0 > ${i}_5.lst ; sleep 120; done
-
-
-
