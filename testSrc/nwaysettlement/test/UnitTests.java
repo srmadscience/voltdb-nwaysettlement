da1863b1419dab7c97f14bc0e01405e270301bae
dwrolfe <david.rolfe@pobix.com>
2025-05-23 12:47:32 +0100

    MIT license

--- testSrc/nwaysettlement/test/UnitTests.java
+++ testSrc/nwaysettlement/test/UnitTests.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2025 Volt Active Data Inc.
+ *
+ * Use of this source code is governed by an MIT
+ * license that can be found in the LICENSE file or at
+ * https://opensource.org/licenses/MIT.
+ */
+
 package nwaysettlement.test;
 
 import static org.junit.jupiter.api.Assertions.fail;
